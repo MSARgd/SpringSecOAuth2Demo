bc92321a8d5886007f177d8f764167a316ef09de
sidiAbdalla <sidabdallard@gmail.com>
2024-02-03 23:19:16 +0100

    Sat Feb  3 11:19:16 PM +01 2024

---
+++ src/test/java/rgd/msa/SpringSecOAuth2Demo/SpringSecOAuth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rgd.msa.SpringSecOAuth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecOAuth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
